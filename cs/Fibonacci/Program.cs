bafd8a84140bd6f1620e5c5b65a7562270584088
Alexander Muravtsev <almuravcev@yandex.ru>
2022-03-09 22:09:22 +0300

    the first experiment with cs is added

---
+++ cs/Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position;
+            Console.Write("Enter Fibonacci number position: ");
+            try
+            {
+                position = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Only integer numbers are accepted");
+                position = 0;
+                System.Environment.Exit(0);
+            }
+            
+            double golden_sec = (1 + Math.Sqrt(5)) / 2;
+
+            double fibonacci_num = (Math.Pow(golden_sec, position) - Math.Pow(-golden_sec, -position)) / 
+            (2 * golden_sec - 1);
+            
+            Console.WriteLine(Convert.ToInt64(fibonacci_num));
+        }
+    }
+}
