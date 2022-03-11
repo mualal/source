9fe7cce7572ac13b9d9495e15682a96ae3c0ec93
Alexander Muravtsev <almuravcev@yandex.ru>
2022-03-11 20:53:36 +0300

    contours detection is added

--- python/opencv_exploration.py
+++ python/opencv_exploration.py
@@ -12,6 +12,8 @@ while capture.isOpened():
     success, frame = capture.read()
     frame = cv2.flip(frame, 1)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    """
     faces = face_cascade.detectMultiScale(frame_gray, 1.3, 5)
     for (x, y, w, h) in faces:
         frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -25,6 +27,23 @@ while capture.isOpened():
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     # cv2.imshow('frame_gray', frame_gray)
+    """
+    _, threshold = cv2.threshold(frame_gray, 127, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+
+        cv2.drawContours(frame, [contour], 0, (255, 0, 0), 1)
+
+        M = cv2.moments(contour)
+        if M['m00'] != 0.0:
+            x = int(M['m10']/M['m00'])
+            y = int(M['m01']/M['m00'])
+
+    cv2.imshow('shapes', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 capture.release()
 cv2.destroyAllWindows()
