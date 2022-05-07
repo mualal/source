89b2bdebcdd0e0b09dfa11ef85c9ad38b0faf0cc
Alexander Muravtsev <almuravcev@yandex.ru>
2022-05-07 18:13:57 +0300

    ev3: fix motors/sensors ports linking

--- ev3/exploration/main.py
+++ ev3/exploration/main.py
@@ -12,33 +12,39 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 
+def wait_and_display_clear(brick):
+    wait(100)
+    brick.screen.clear()
+
+
 # Create your objects here.
 ev3 = EV3Brick()
 test_motor = Motor(Port.A)
-test_eyes = UltrasonicSensor(Port.B)
-test_touch_sensor = TouchSensor(Port.C)
-test_gyro_sensor = GyroSensor(Port.D)
-
+test_eyes = InfraredSensor(Port.S1)
+test_touch_sensor = TouchSensor(Port.S2)
+test_gyro_sensor = GyroSensor(Port.S3)
 
 # Write your program here.
 ev3.speaker.beep()
 
 while True:
+    ev3.screen.draw_text(10, 60, 'Dist: ' + str(test_eyes.distance()) + ' cm')
 
-    print(test_eyes.distance())
-
-    if test_eyes.distance() < 500:
+    if test_eyes.distance() < 50:
         test_motor.run_target(500, 90)
 
     if test_touch_sensor.pressed():
         break
 
-    wait(100)
+    wait_and_display_clear(ev3)
 
 ev3.speaker.beep(frequency=1000, duration=500)
 
 while True:
-    
-    print(test_gyro_sensor.speed())
+        
+    ev3.screen.draw_text(10, 60, 'Gyro: ' + str(test_gyro_sensor.speed()))
 
-    wait(100)
+    if test_touch_sensor.pressed():
+        break
+
+    wait_and_display_clear(ev3)
